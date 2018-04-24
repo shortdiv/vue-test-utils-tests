d4bf709eaf6ddcc0c33f630608eafa7623daa454
divya <hello@shortdiv.com>
2018-04-24 15:54:55 -0500

    Wrapped update check in next tick to ensure that I wasn't missing the event call

--- test/specs/component-lifecycle.spec.js
+++ test/specs/component-lifecycle.spec.js
@@ -1,15 +1,26 @@
 import { mount } from '@vue/test-utils'
+import Vue from 'vue'
 import sinon from 'sinon'
 import ComponentWithLifecycle from '../utils/componentWithLifecycle'
 
 describe('component with lifecycle', () => {
   it('should call updated when data is set', () => {
     const updateEvent = sinon.stub()
-    const wrapper = mount(ComponentWithLifecycle, {
-      propsData: { updateEvent }
+    const vm = new Vue({
+      template: `<div>{{ name }}</div>`,
+      data () {
+        return {
+          name: 'evan'
+        }
+      },
+      updated () {
+        updateEvent()
+      }
+    }).$mount()
+    vm.$set(vm, 'name', 'Evan You')
+    vm.$nextTick(() => {
+      expect(vm.name).toBe('Evan You')
+      expect(updateEvent.called).toBe(true)
     })
-    wrapper.vm.$set(wrapper.vm, 'name', 'Evan You')
-    expect(wrapper.vm.name).toBe('Evan You')
-    expect(updateEvent.called).toBe(true)
   })
 })
