cb1bfce3e9ff9b66e4bd66166ef5d621e3db7737
divya <hello@shortdiv.com>
2018-04-24 15:59:05 -0500

    Checking that mounted lifecycle hook is called

--- test/specs/component-lifecycle.spec.js
+++ test/specs/component-lifecycle.spec.js
@@ -6,12 +6,16 @@ import ComponentWithLifecycle from '../utils/componentWithLifecycle'
 describe('component with lifecycle', () => {
   it('should call updated when data is set', () => {
     const updateEvent = sinon.stub()
+    const mountedEvent = sinon.stub()
     const vm = new Vue({
       template: `<div>{{ name }}</div>`,
       data () {
         return {
           name: 'evan'
         }
+      },
+      mounted () {
+        mountedEvent()
       },
       updated () {
         updateEvent()
@@ -20,6 +24,7 @@ describe('component with lifecycle', () => {
     vm.$set(vm, 'name', 'Evan You')
     vm.$nextTick(() => {
       expect(vm.name).toBe('Evan You')
+      expect(mountedEvent.called).toBe(true)
       expect(updateEvent.called).toBe(true)
     })
   })
